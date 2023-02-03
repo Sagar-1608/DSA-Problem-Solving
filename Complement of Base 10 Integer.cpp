35868536fa8a7941bf5d9f1b0380b63dab2867d4
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-02-03 18:27:15 +0530

    Create Complement of Base 10 Integer.cpp

---
+++ Complement of Base 10 Integer.cpp
@@ -0,0 +1,28 @@
+//Complement of Base 10 Integer
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        
+        
+        
+            
+            int m= n;
+            int mask = 0;
+        if (n==0)
+        {
+            return 1;        }
+            
+            while(n!=0)
+            {
+               
+                mask= (mask<<1)|1;
+                    
+                n=n>>1;
+            }
+            
+        int ans = (~m)&mask;
+        return ans;
+        
+        
+    }
+};
