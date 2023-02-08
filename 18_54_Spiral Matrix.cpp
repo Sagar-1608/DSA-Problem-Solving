a42e7cf383cf888c4815d6729a2394ab6adb1fa4
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-02-08 23:42:39 +0530

    Create 18_54_Spiral Matrix.cpp

---
+++ 18_54_Spiral Matrix.cpp
@@ -0,0 +1,62 @@
+//54. Spiral Matrix
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        
+        vector<int> ans;
+      
+        int row= matrix.size();
+        int col= matrix[0].size();
+        
+        int count=0;
+        int total = row*col;
+        
+        int startRow = 0;
+        int startCol = 0;
+        int endRow = row-1;
+        int endCol  = col-1; 
+        
+        while(count<total)
+        {  
+            // printing starting row 
+            
+            for(int i=startCol;  i<=endCol  && count<total;  i++)
+            {
+                ans.push_back(matrix[startRow][i]);
+                count++;
+            }
+            startRow++;
+            
+            //printing ending column 
+            
+            for(int i=startRow;  i<=endRow &&  count<total;  i++)
+            {
+                ans.push_back(matrix[i][endCol ]);
+                count++;
+            }
+            endCol--;
+            
+            
+            //print ending row
+            for(int i=endCol;  i>=startCol &&  count<total;  i--)
+            {
+                ans.push_back(matrix[endRow][i]);
+                count++;
+            }
+            endRow--;
+            
+            // printing start column
+            
+            for(int i=endRow;  i>=startRow &&  count<total;  i--)
+            {
+                ans.push_back(matrix[i][startCol]);
+                count++;
+            }
+            startCol++;
+            
+                
+        }
+    return ans;    
+        
+    }
+};
