47ba3bdc8bc7b9b597de6043ea8c35076413f213
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-02-10 00:16:32 +0530

    Create 28_1491_Average Salary Excluding the Minimum and Maximum Salary.cpp

---
+++ 28_1491_Average Salary Excluding the Minimum and Maximum Salary.cpp
@@ -0,0 +1,20 @@
+//1491 Average Salary Excluding the Minimum and Maximum Salary
+class Solution {
+public:
+    double average(vector<int>& arr) {
+
+        sort(arr.begin(),arr.end());
+        double sum=0;
+      
+        for(int i = 1; i< arr.size()-1; i++)
+        {
+
+            sum = sum + arr[i];
+        }
+        
+        
+
+        return double(sum/(arr.size()-2));
+        
+    }
+};
