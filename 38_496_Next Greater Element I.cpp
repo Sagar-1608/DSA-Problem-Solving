3643490c8ffbfd37d41f29a7b2081b58b1ced328
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-02-13 23:56:07 +0530

    Create 38_496_Next Greater Element I.cpp

---
+++ 38_496_Next Greater Element I.cpp
@@ -0,0 +1,36 @@
+// 496. Next Greater Element I
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& n1, vector<int>& n2) {
+
+       vector<int> ans ;
+        for (int i =0 ; i<n1.size(); i++)
+        {
+            int ind=0;
+            for (int j=0; j< n2.size()  ; j++)
+            {
+                if(n1[i]==n2[j])
+                {
+                    ind=j;
+                }
+
+                
+            }
+            int ind2=0;
+            for(int p =ind+1; p<n2.size();p++)
+            {
+                if(n1[i]<n2[p])
+                {
+                      ind2=p;
+                    break;
+                }
+            }
+
+        if( ind2==0) ans.push_back(-1);
+        else ans.push_back(n2[ind2]);
+        }
+        
+        return ans;
+    }
+};
