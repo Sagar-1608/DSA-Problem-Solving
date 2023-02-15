bac46598251231cbea874655e76ab8833d740878
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-02-15 23:55:47 +0530

    Create 41_283_Move Zeroes.cpp

---
+++ 41_283_Move Zeroes.cpp
@@ -0,0 +1,22 @@
+// 283. Move Zeroes
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        
+        int n=nums.size();
+        int nonzero=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]!=0)
+            {
+                swap(nums[nonzero],nums[i]);
+                nonzero++;
+                     
+                     
+            }
+        }
+        
+    }
+};
