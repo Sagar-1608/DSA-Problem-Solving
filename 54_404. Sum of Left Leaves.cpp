02cecbd077342a33fb7f0b111bab4802c82fb42e
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-02-17 23:31:02 +0530

    Create 54_404. Sum of Left Leaves.cpp

---
+++ 54_404. Sum of Left Leaves.cpp
@@ -0,0 +1,33 @@
+// 404. Sum of Left Leaves
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int total=0;
+
+  void dfs(TreeNode* root )
+{
+    if(root==NULL) return;
+    if(root->left!=NULL && (root->left->left ==NULL && root->left->right==NULL))
+    {
+         total+=root->left->val;
+    }
+    dfs(root->left);
+    dfs(root->right);
+        
+}
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        dfs(root);
+        return total;
+        
+    }
+};
