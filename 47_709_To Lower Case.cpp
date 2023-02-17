e41de510811095b9a4af953a551213f6e43a9479
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-02-17 23:11:06 +0530

    Create 47_709_To Lower Case.cpp

---
+++ 47_709_To Lower Case.cpp
@@ -0,0 +1,13 @@
+// 709. To Lower Case
+class Solution {
+public:
+    string toLowerCase(string s) {
+        
+        for (int i=0; i<s.length(); i++)
+        {
+            if (isupper(s[i]))
+            s[i] = tolower(s[i]); 
+        }
+        return s;
+    }
+};
