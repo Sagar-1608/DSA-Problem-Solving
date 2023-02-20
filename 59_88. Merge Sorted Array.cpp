a0c6490256b15cc7510e98b41063b24c410ef2e4
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-02-20 23:23:34 +0530

    Create 59_88. Merge Sorted Array.cpp

---
+++ 59_88. Merge Sorted Array.cpp
@@ -0,0 +1,46 @@
+// 88. Merge Sorted Array
+
+
+class Solution {
+
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+        int i=0,j=0;
+        vector <int > result;
+        
+        
+        while (i<m &&j<n)
+        {
+            if(nums1[i]<nums2[j])
+            {
+                result.push_back(nums1[i]);
+                i++;
+                
+            }
+            else
+            {
+             result.push_back(nums2[j]);
+                j++;   
+            }
+        }
+        
+        while (i<m)
+        {
+           result.push_back(nums1[i]);
+                i++; 
+        }
+        
+            while (j<n)
+        {
+            result.push_back(nums2[j]);
+                j++;    
+            }
+        nums1.resize(m+n,0);
+        for(int i=0 ;i<(m+n);i++)
+        {
+            nums1[i]=result[i];
+        }
+    }
+};
+        
