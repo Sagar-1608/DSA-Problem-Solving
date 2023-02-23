537443f99ef862f7fa301462c6488f6b9df0aff6
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-02-23 23:57:20 +0530

    Create 71_383. Ransom Note.cpp

---
+++ 71_383. Ransom Note.cpp
@@ -0,0 +1,19 @@
+// 383. Ransom Note
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+
+        map<char, int > m1,m2;
+        for(auto i: ransomNote) m1[i]++;
+        for(auto i:  magazine) m2[i]++;
+
+        for(auto i : m1)
+        {
+            if(i.second>m2[i.first]) return false;
+        }
+        return true ;
+
+        
+    }
+};
