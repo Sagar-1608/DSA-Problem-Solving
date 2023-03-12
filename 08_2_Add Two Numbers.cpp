bd945674b500b111fc9dd6ce0f8e7a74f53a0085
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-03-12 23:52:14 +0530

    Update 08_2_Add Two Numbers.cpp
    optimal solution is added

--- 08_2_Add Two Numbers.cpp
+++ 08_2_Add Two Numbers.cpp
@@ -1,6 +1,7 @@
 //Add Two Numbers
 
-/**
+/*
+*
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
@@ -10,7 +11,7 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+/*class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
      
@@ -92,6 +93,88 @@ public:
        
         
         return ans->next;
+        
+    }
+};
+*/
+
+updated code is here
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+   int getlen(ListNode *head)
+   { 
+       long c=0;
+       while(head)
+       {
+           c++;
+           head=head->next;
+       }
+       return c;
+   }
+   ListNode * add(ListNode* &l1, ListNode* l2 )
+   {   
+       ListNode * ans =new ListNode(-1);
+       ans->next=l1;
+       ListNode *prev=NULL;
+
+      int carry =0;
+       while(l1 || l2 )
+       {
+        prev=l1;
+         int val1=0;
+         int val2=0;
+
+         if(l1!=NULL) val1=l1->val;
+         if(l2!=NULL) val2=l2->val;
+
+
+         int sum =carry + val1 + val2;
+         int digit = sum%10;
+         l1->val=digit;
+         carry =sum/10;
+
+         if(l1!=NULL) l1=l1->next;
+         if(l2!=NULL) l2=l2->next;
+       }
+
+       if(carry>0)
+       {
+           ListNode * carr=new ListNode(carry);
+           prev->next=carr;
+
+       }
+       return ans->next;
+   }
+
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+     
+        int len1 = getlen(l1);
+        int len2 = getlen(l2);
+        ListNode* ans;
+
+        if(len1>len2)
+        {
+              ans= add(l1,l2 );
+        }
+        else
+        {
+             ans= add(l2,l1 );
+
+        }
+        return ans;
+
+        
         
     }
 };
